6a9616892c81c3a83765d94ca6fbc763fe00d07a
Luke Diebold <luke@ldiebold.com>
2022-04-01 19:15:04 +1000

    initial commit

---
+++ src/contracts/crud/UseCreateModel.ts
@@ -0,0 +1,7 @@
+export interface UseCreateModelReturn<Model> {
+
+}
+
+export type UseCreateModel<Model> = () => UseCreateModelReturn<Model>
+
+export default UseCreateModel
---
+++ src/contracts/crud/UseModel.ts
@@ -0,0 +1,21 @@
+import { ComputedRef, Ref } from "vue-demi"
+import { Model, ApiError } from "types"
+
+export interface UseModelReturn<M extends Model> {
+  create: (customForm?: Partial<any>) => Promise<void>
+  find: () => Promise<void>
+  update: () => Promise<void>
+  remove: () => Promise<void>
+  model: ComputedRef<Item<InstanceOf<M>>>
+  form: Partial<any>
+  resetForm: () => void
+  id: Ref<string | number | null>
+  error: Ref<ApiError | null>
+  creating: Ref<boolean>
+  finding: Ref<boolean>
+  updating: Ref<boolean>
+  removing: Ref<boolean>
+  loading: Ref<boolean>
+}
+
+export type UseModel<M extends Model> = () => UseModelReturn<M>
